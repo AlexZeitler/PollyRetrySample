95272035b625d9dc0988072179207a11dfe7370b
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-05-04 10:25:20 +0200

    test: policy retries on not existing site, then throws

---
+++ PollyHttpClientRetry.Tests/PollyRetryLearningTests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Polly;
+using Xunit;
+
+namespace PollyHttpClientRetry.Tests;
+
+public class PollyHttpClientRetryTests
+{
+  [Fact]
+  public async Task ShouldRetryOnClosedSocket()
+  {
+    const int maxRetryCount = 5;
+    const double durationBetweenRetries = 250;
+
+    var actualRetries = 0;
+
+    var retryPolicy = Policy
+      .Handle<HttpRequestException>()
+      .WaitAndRetryAsync(
+        maxRetryCount,
+        retryCount =>
+        {
+          actualRetries = retryCount;
+          return TimeSpan.FromMilliseconds(
+            durationBetweenRetries * Math.Pow(
+              2,
+              retryCount - 1
+            )
+          );
+        }
+      );
+
+    var httpClient = new HttpClient();
+    var url = new Uri("http://timeout.alexanderzeitler.com");
+    try
+    {
+      using var response = await retryPolicy.ExecuteAsync(
+        () => httpClient.SendAsync(
+          new HttpRequestMessage
+          {
+            Method = HttpMethod.Get,
+            RequestUri = url
+          }
+        )
+      );
+      Assert.Equal(
+        maxRetryCount,
+        actualRetries
+      );
+    }
+    catch (Exception e)
+    {
+      Assert.Equal(
+        maxRetryCount,
+        actualRetries
+      );
+    }
+  }
+}
